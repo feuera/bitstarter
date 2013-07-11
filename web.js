2bfa5454ee61986f44eb71057e21c97e3101beb3
Andi <feuera@gmail.com>
2013-07-11 17:50:42 +0200

    read buffer

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var htxt = fs.readFileSync('hello.html');
+    response.send(htxt);
+    //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
